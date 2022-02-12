a284f0b65a6f30b349b9ef7d419edde481f9df28
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-02-12 13:43:06 -0300

    Migrations

---
+++ app_gestao/database/migrations/2022_02_12_011145_alter_produtos_adicionar_relacionamento_unidades.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterProdutosAdicionarRelacionamentoUnidades extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+    }
+}
