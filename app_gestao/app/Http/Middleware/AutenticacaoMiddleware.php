2dec510cdfad3ceac14b49440483ea2194f9287f
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-02-14 20:41:49 -0300

    Middlewares

---
+++ app_gestao/app/Http/Middleware/AutenticacaoMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Facade\FlareClient\Http\Response;
+
+class AutenticacaoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
+    {
+        return Response('Acesso Negado! Rota exige autenticação.');
+    }
+}
